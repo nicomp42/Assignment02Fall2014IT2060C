28f180534b59fb9464c033b95115d41b57a26a5b
Bill Nicholson <GitHub@NicholsonComputerConsulting.com>
2014-08-31 21:38:58 -0400

    Nice progress

--- Assignment02Fall2014IT2060C/src/assignment02/Assignment02.java
+++ Assignment02Fall2014IT2060C/src/assignment02/Assignment02.java
@@ -1,5 +1,17 @@
 package assignment02;
 
 public class Assignment02 {
-
+	public Boolean IsPrime(int num) {
+		Boolean isPrime = true;
+		int limit = (int) Math.sqrt(num);
+		
+		for (int i = 2; i < limit; i++) {
+			if ((num % i) == 0) {
+				isPrime = false;
+				break;
+			}
+		}
+		
+		return isPrime;
+	}
 }
--- Assignment02Fall2014IT2060C/src/main/Main.java
+++ Assignment02Fall2014IT2060C/src/main/Main.java
@@ -1,10 +1,24 @@
 package main;
 
+import assignment02.Assignment02;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Assignment02 assignment02 = new Assignment02();
+		int test = 42;
+		System.out.println(test + " : " + assignment02.IsPrime(test));
 
+		test = 43;
+		System.out.println(test + " : " + assignment02.IsPrime(test));
+
+		test = 17;
+		System.out.println(test + " : " + assignment02.IsPrime(test));
+
+		test = 3;
+		System.out.println(test + " : " + assignment02.IsPrime(test));
+		
+		test = 101;
+		System.out.println(test + " : " + assignment02.IsPrime(test));
 	}
-
 }
