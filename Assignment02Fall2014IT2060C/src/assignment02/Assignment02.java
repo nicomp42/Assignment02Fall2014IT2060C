34c40c72ab1cb43c5fe13c20ab9a1c706dc9333a
Bill Nicholson <GitHub@NicholsonComputerConsulting.com>
2014-08-31 21:59:18 -0400

    Added comments to Assignment02.java

--- Assignment02Fall2014IT2060C/src/assignment02/Assignment02.java
+++ Assignment02Fall2014IT2060C/src/assignment02/Assignment02.java
@@ -1,6 +1,12 @@
 package assignment02;
 
+
 public class Assignment02 {
+	/**
+	 * Test a number for primeness
+	 * @param num the number to be tested
+	 * @return True if num is prime, false otherwise.
+	 */
 	public Boolean IsPrime(int num) {
 		Boolean isPrime = true;
 		int limit = (int) Math.sqrt(num);
