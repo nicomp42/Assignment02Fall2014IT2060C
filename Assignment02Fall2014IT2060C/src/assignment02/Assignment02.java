342c303b291f0eff26cde02943e0316ea97b23d3
Bill Nicholson <GitHub@NicholsonComputerConsulting.com>
2014-09-01 06:48:00 -0400

    More cool stuff

--- Assignment02Fall2014IT2060C/src/assignment02/Assignment02.java
+++ Assignment02Fall2014IT2060C/src/assignment02/Assignment02.java
@@ -7,7 +7,7 @@ package assignment02;
  */
 public class Assignment02 {
 	/**
-	 * Test a number for primeness
+	 * Test a number for primeness using a brute-force loop
 	 * @param num the number to be tested
 	 * @return True if num is prime, false otherwise.
 	 */
@@ -21,7 +21,34 @@ public class Assignment02 {
 				break;
 			}
 		}
+		return isPrime;
+	}
+	/**
+	 * Test a number for primeness using a list of primes, then a brute-force loop
+	 * @param num the number to be tested
+	 * @return True if num is prime, false otherwise.
+	 */
+	public Boolean IsPrimeV02(int num) {
+		final int[] primeList = new int[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 27, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157};
+		Boolean isPrime = true;
+		int limit = (int) Math.sqrt(num);
+		int myPrime = 0;
 		
+		for (int i = 2; i < primeList.length; i++) {
+			myPrime = primeList[i];
+			if ((num % myPrime) == 0) {
+				isPrime = false;
+				break;
+			}
+		}
+		if (isPrime == true) {
+			for (int i = myPrime + 2; i < limit; i++) {
+				if ((num % i) == 0) {
+					isPrime = false;
+					break;
+				}
+			}
+		}
 		return isPrime;
 	}
 }
